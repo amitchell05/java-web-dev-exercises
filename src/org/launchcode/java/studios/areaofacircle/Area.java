79fff6ae695f98dc61fda1bf746e722f6776067c
Angela Mitchell <anglarenmit@gmail.com>
2022-03-12 12:47:57 -0600

    validate if radius is less than 0

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a radius: ");
+        Double radius = input.nextDouble();
 
-        double radius = input.nextDouble();
+        boolean isValidRadius = validateRadius(radius);
 
-        double area = Circle.getArea(radius);
+        if (isValidRadius) {
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " +  radius + " is: " + area);
+        }
+    }
 
-        System.out.println("The area of a circle of radius " +  radius + " is: " + area);
+    static boolean validateRadius(Double radius) {
+        if (radius < 0) {
+            System.out.println("Please enter a radius that is greater than or equal to 0.");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
