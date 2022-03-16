d08bae5290485a024536a47d26000bd922326553
Angela Mitchell <anglarenmit@gmail.com>
2022-03-15 21:11:49 -0500

    make character counts case insensitive

--- src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
+++ src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
 public class CharacterCounter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        HashMap<Character, Integer> storedChars = new HashMap<>();
+        HashMap<String, Integer> storedChars = new HashMap<>();
 
         System.out.print("Enter a quote: ");
 
         String quote = input.nextLine();
-//        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
         for (char character : quote.toCharArray()) {
-            if (storedChars.containsKey(character)) {
-                storedChars.put(character, storedChars.get(character) + 1);
+            String convertedChar = Character.toString(character).toLowerCase();
+            if (storedChars.containsKey(convertedChar)) {
+                storedChars.put(convertedChar, storedChars.get(convertedChar) + 1);
             } else {
-                storedChars.put(character, 1);
+                storedChars.put(convertedChar, 1);
             }
         }
 
-        for (Map.Entry<Character, Integer> character : storedChars.entrySet()) {
+        for (Map.Entry<String, Integer> character : storedChars.entrySet()) {
             System.out.println(character.getKey() + ": " + character.getValue());
         }
     }
