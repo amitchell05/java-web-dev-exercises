7f03837566b7fc9dd8b44af22c6e0643fbf4c2f4
Angela Mitchell <anglarenmit@gmail.com>
2022-03-15 20:16:03 -0500

    prompt user for a string

--- src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
+++ src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
@@ -1,13 +1,18 @@
 package org.launchcode.java.studios.countcharacters;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CharacterCounter {
     public static void main(String[] args) {
-        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);
         HashMap<Character, Integer> storedChars = new HashMap<>();
+
+        System.out.print("Enter a quote: ");
+
+        String quote = input.nextLine();
+//        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 
         for (char character : quote.toCharArray()) {
             if (storedChars.containsKey(character)) {
