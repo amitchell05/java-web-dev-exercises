cdb4fd5d1307e770a84dff5bfdb4cf7afe60015e
Angela Mitchell <anglarenmit@gmail.com>
2022-03-15 19:14:51 -0500

    complete alternative solution for studio 2

--- src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
+++ src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
@@ -9,11 +9,11 @@ public class CharacterCounter {
         String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         HashMap<Character, Integer> storedChars = new HashMap<>();
 
-        for (int i = 0; i < quote.length(); i++) {
-            if (storedChars.containsKey((quote.charAt(i)))) {
-                storedChars.put(quote.charAt(i), storedChars.get(quote.charAt(i)) + 1);
+        for (char character : quote.toCharArray()) {
+            if (storedChars.containsKey(character)) {
+                storedChars.put(character, storedChars.get(character) + 1);
             } else {
-                storedChars.put(quote.charAt(i), 1);
+                storedChars.put(character, 1);
             }
         }
 
