a1842d4e3fcb08af5a98d94edc9905b83fe85090
Angela Mitchell <anglarenmit@gmail.com>
2022-03-15 22:11:53 -0500

    read string from a file

--- src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
+++ src/org/launchcode/java/studios/countcharacters/CharacterCounter.java
@@ -1,17 +1,20 @@
 package org.launchcode.java.studios.countcharacters;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class CharacterCounter {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
         HashMap<String, Integer> storedChars = new HashMap<>();
 
-        System.out.print("Enter a quote: ");
+        Path directory = Paths.get("C:\\Users\\Yoga 2 Mitchell\\IdeaProjectsTemp\\src\\org\\launchcode\\java\\studios\\countcharacters");
+        Path quotePath = directory.resolve("quote.txt");
 
-        String quote = input.nextLine();
+        String quote = Files.readString(quotePath);
 
         for (char character : quote.toCharArray()) {
             String convertedChar = Character.toString(character).toLowerCase();
